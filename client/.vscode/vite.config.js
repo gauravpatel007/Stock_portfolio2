7b3ca072d0012ebfa77b734ab46550111e310ab9
Gaurav Patel <gaurav.patel.1008.2006@gmail.com>
2026-07-25 23:24:53 +0530

    Remove node_modules from GitHub

--- client/.vscode/vite.config.js
+++ client/.vscode/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Stock_portfolio2/",
   plugins: [
     react(),
     tailwindcss(),
